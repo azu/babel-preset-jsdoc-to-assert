5da7147e0982e97a139cfc4ad5ec98bd74f8964b
azu <azuciao@gmail.com>
2016-08-16 09:26:46 +0900

    Breaking Change: update babel-plugin-jsdoc-to-assert@2

--- test/fixtures/FunctionDeclaration/expected.js
+++ test/fixtures/FunctionDeclaration/expected.js
@@ -4,6 +4,6 @@
  * @param {string[]} [c] - this is a param.
  */
 function myFunc(param, b, c) {
-  console.assert(typeof param === "number", 'Invalid JSDoc @param: typeof param === "number"');
-  console.assert(typeof b === "string", 'Invalid JSDoc @param: typeof b === "string"');
+  console.assert(typeof param === "number", 'Invalid JSDoc: typeof param === "number"');
+  console.assert(typeof b === "string", 'Invalid JSDoc: typeof b === "string"');
 }
