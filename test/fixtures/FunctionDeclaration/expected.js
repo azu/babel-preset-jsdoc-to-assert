3af00f1ac777f38785ab1877a326d452ebf526d9
azu <azuciao@gmail.com>
2016-08-29 19:42:06 +0900

    fix(test): update test case

--- test/fixtures/FunctionDeclaration/expected.js
+++ test/fixtures/FunctionDeclaration/expected.js
@@ -4,6 +4,13 @@
  * @param {string[]} [c] - this is a param.
  */
 function myFunc(param, b, c) {
-  console.assert(typeof param === "number", 'Invalid JSDoc: typeof param === "number"');
-  console.assert(typeof b === "string", 'Invalid JSDoc: typeof b === "string"');
+  if (!(typeof param === "number")) {
+    console.error('TypeError: babel-plugin-jsdoc-to-assert\nExpected type: @param {number} param\nActual value:', param);
+    console.assert(typeof param === "number", 'Invalid JSDoc: typeof param === "number"');
+  }
+
+  if (!(typeof b === "string")) {
+    console.error('TypeError: babel-plugin-jsdoc-to-assert\nExpected type: @param {string} b\nActual value:', b);
+    console.assert(typeof b === "string", 'Invalid JSDoc: typeof b === "string"');
+  }
 }
