89dfec649a0e3dee45b8c0ac8ed382aaf28ec638
azu <azu@users.noreply.github.com>
2018-09-16 16:36:22 +0900

    feat(babel): upgrade to babel 7 (#2)
    BREAKING CHANGE: require babel 7>=

--- test/babel-preset-jsdoc-to-assert-test.js
+++ test/babel-preset-jsdoc-to-assert-test.js
@@ -1,18 +1,18 @@
 const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
-const plugin = require("../lib/babel-preset-jsdoc-to-assert");
-const transformFileSync = require("babel-core").transformFileSync;
+const transformFileSync = require("@babel/core").transformFileSync;
+
 function trim(str) {
     return str.replace(/^\s+|\s+$/, '');
 }
 
 const fixturesDir = path.join(__dirname, 'fixtures');
-describe('finds JSDoc', function () {
-    fs.readdirSync(fixturesDir).map(function (caseName) {
-        it("should " + caseName.split('-').join(' '), function () {
+describe('finds JSDoc', function() {
+    fs.readdirSync(fixturesDir).map(function(caseName) {
+        it("should " + caseName.split('-').join(' '), function() {
             const fixtureDir = path.join(fixturesDir, caseName);
-            var actualPath = path.join(fixtureDir, 'actual.js');
+            let actualPath = path.join(fixtureDir, 'actual.js');
             const actual = transformFileSync(actualPath).code;
 
             if (path.sep === '\\') {
@@ -20,10 +20,17 @@ describe('finds JSDoc', function () {
                 actualPath = actualPath.replace(/\\/g, '/');
             }
 
+
+            const expectedPath = path.join(fixtureDir, 'expected.js');
             const expected = fs.readFileSync(
-                path.join(fixtureDir, 'expected.js')
+                expectedPath
             ).toString().replace(/%FIXTURE_PATH%/g, actualPath);
 
+            if (process.env.UPDATE_SNAPSHOT) {
+                fs.writeFileSync(expectedPath, actual);
+                this.skip();
+                return;
+            }
             assert.equal(trim(actual), trim(expected));
         });
     });
--- test/fixtures/FunctionDeclaration/expected.js
+++ test/fixtures/FunctionDeclaration/expected.js
@@ -4,11 +4,6 @@
  * @param {string[]} [c] - this is a param.
  */
 function myFunc(param, b, c) {
-  if (!(typeof param === "number")) {
-    console.assert(typeof param === "number", 'Expected type: @param {number} param\nActual value:', param, '\nFailure assertion: typeof param === "number"');
-  }
-
-  if (!(typeof b === "string")) {
-    console.assert(typeof b === "string", 'Expected type: @param {string} b\nActual value:', b, '\nFailure assertion: typeof b === "string"');
-  }
+  console.assert(typeof param === "number", 'Expected type: @param {number} param\nActual value:', param, '\nFailure assertion: typeof param === "number"');
+  console.assert(typeof b === "string", 'Expected type: @param {string} b\nActual value:', b, '\nFailure assertion: typeof b === "string"');
 }
